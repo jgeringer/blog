843da4b9684809c1d48ec7bfb535844f26ee7695
Joseph Geringer <joegeringer@gmail.com>
2021-08-27 17:53:26 -0500

    feat(Stats): started styling table

--- my-first-app/src/components/Sidebar.tsx
+++ my-first-app/src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 // @ts-nocheck
 import React, { useState, useEffect, useCallback } from 'react';
-import { List, ListItem, Note, Paragraph } from '@contentful/forma-36-react-components';
+import { List, ListItem, Note, Paragraph, Table, TableRow, TableCell, TableHead } from '@contentful/forma-36-react-components';
 import { SidebarExtensionSDK } from '@contentful/app-sdk';
 
 interface SidebarProps {
@@ -15,7 +15,7 @@ const Sidebar = (props: SidebarProps) => {
 
   const contentFieldSlug = sdk.entry.fields[CONTENT_FIELD_ID_SLUG];
   
-  const [statText, setStatText] = useState(null);
+  const [statText, setStatText] = useState([]);
   
   function getStatsEntry() {
     return new Promise(resolve => {
@@ -25,30 +25,72 @@ const Sidebar = (props: SidebarProps) => {
     });
   }
 
+// [
+//   {
+//       "type": [
+//           {
+//               "title": "GA Stats - 1 month",
+//               "data": {
+//                   "Pageviews": "93",
+//                   "Last update": "Wed, 08/25/2021 - 04:20",
+//                   "Unique Pageviews": "42",
+//                   "Average Time on Page (sec)": "56"
+//               }
+//           },
+//           {
+//               "title": "GA Stats - 5 years",
+//               "data": {
+//                   "Pageviews": "5000",
+//                   "Last update": "Wed, 08/25/2021 - 06:00",
+//                   "Unique Pageviews": "2175",
+//                   "Average Time on Page (sec)": "57"
+//               }
+//           }
+//       ]
+//   }
+// ]
+
   useEffect(() => {
+    sdk.window.startAutoResizer();
     (async function() {
       const stats = await getStatsEntry();
-      console.log(`stats: `, stats);
-      console.log(`stats title: `, stats.fields.stats["en-US"].reporting[0].type[0].title);
-      setStatText(stats.fields.stats["en-US"].reporting[0].type[0].title);
+      console.log(`dummy stats: `, stats);
+      setStatText(stats.fields.stats["en-US"]);
     })();
   }, []);
 
-
+  const renderTable = (item) => {
+    for (const [key, value] of Object.entries(item)) {
+      return (
+        <TableRow>
+          <TableCell>{key}</TableCell>
+          <TableCell>{value}</TableCell>
+        </TableRow>
+      )
+    }
+  }
+  
+  
   // Render the metrics with Forma36 components
   return (
     <>
       <Note style={{ marginBottom: '12px' }}>
-        <Paragraph>TODO:</Paragraph>
-        <List>
-                     
-          <div>
-            data: {statText}
-          </div>
+        <Paragraph>URL Slug:<br />{contentFieldSlug.getValue()}</Paragraph><br />
 
-          <ListItem>2! - X - Autosize this iframe</ListItem>
-          <ListItem>Read slug from current page: <strong>{contentFieldSlug.getValue()}</strong></ListItem>
-        </List>
+          {statText.map(stat => (
+            <Table>
+              <TableHead>{stat.title}</TableHead>
+              {stat.data && (
+                <>
+                  {console.log(`dummy man: `, stat.data)}
+                  {console.log(`dummy man 2: `, JSON.stringify(stat.data[0]))}
+                  {(stat.data).map(item => (
+                    renderTable(item)
+                  ))}
+                </>
+              )}
+            </Table>
+          ))}
       </Note>
     </>
   );
