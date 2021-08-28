ef4581986f97babfbb81ec0eedef7520e7123442
Joseph Geringer <joegeringer@gmail.com>
2021-08-28 10:51:00 -0500

    feat(Stats): finished styling

--- my-first-app/src/components/Sidebar.tsx
+++ my-first-app/src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 // @ts-nocheck
-import React, { useState, useEffect, useCallback } from 'react';
-import { List, ListItem, Note, Paragraph, Table, TableRow, TableCell, TableHead } from '@contentful/forma-36-react-components';
+import React, { useEffect } from 'react';
+import { Icon, Paragraph, Table, TableRow, TableCell, TableHead } from '@contentful/forma-36-react-components';
 import { SidebarExtensionSDK } from '@contentful/app-sdk';
 
 interface SidebarProps {
@@ -8,90 +8,65 @@ interface SidebarProps {
 }
 
 const CONTENT_FIELD_ID_SLUG = 'slug';
-const CONTENT_FIELD_ID_STATS_JSON = 'XRrVdhk6oe9sMgIW74VaZ';
+const CONTENT_FIELD_ID_STATS_JSON = 'stats';
 
 const Sidebar = (props: SidebarProps) => {
   const { sdk } = props;
 
   const contentFieldSlug = sdk.entry.fields[CONTENT_FIELD_ID_SLUG];
-  
-  const [statText, setStatText] = useState([]);
-  
-  function getStatsEntry() {
-    return new Promise(resolve => {
-      sdk.space.getEntry('XRrVdhk6oe9sMgIW74VaZ').then((data) => {
-        resolve(data);
-      });
-    });
-  }
+  const contentFieldJSON = sdk.entry.fields[CONTENT_FIELD_ID_STATS_JSON];
 
-// [
-//   {
-//       "type": [
-//           {
-//               "title": "GA Stats - 1 month",
-//               "data": {
-//                   "Pageviews": "93",
-//                   "Last update": "Wed, 08/25/2021 - 04:20",
-//                   "Unique Pageviews": "42",
-//                   "Average Time on Page (sec)": "56"
-//               }
-//           },
-//           {
-//               "title": "GA Stats - 5 years",
-//               "data": {
-//                   "Pageviews": "5000",
-//                   "Last update": "Wed, 08/25/2021 - 06:00",
-//                   "Unique Pageviews": "2175",
-//                   "Average Time on Page (sec)": "57"
-//               }
-//           }
-//       ]
-//   }
-// ]
+  const statText = contentFieldJSON.getValue() || [];
 
   useEffect(() => {
-    sdk.window.startAutoResizer();
-    (async function() {
-      const stats = await getStatsEntry();
-      console.log(`dummy stats: `, stats);
-      setStatText(stats.fields.stats["en-US"]);
-    })();
-  }, []);
+    sdk.window.startAutoResizer();    
+  }, []); 
 
-  const renderTable = (item) => {
-    for (const [key, value] of Object.entries(item)) {
-      return (
+  const renderTable = (object) => {
+    const elements = [];
+    for (const property in object) {
+      elements.push(
         <TableRow>
-          <TableCell>{key}</TableCell>
-          <TableCell>{value}</TableCell>
+          <TableCell>{property}</TableCell>
+          <TableCell>{object[property]}</TableCell>
         </TableRow>
       )
     }
+    return (elements)
   }
   
-  
-  // Render the metrics with Forma36 components
   return (
     <>
-      <Note style={{ marginBottom: '12px' }}>
-        <Paragraph>URL Slug:<br />{contentFieldSlug.getValue()}</Paragraph><br />
+      <div>
+        <Icon              
+          icon="InfoCircle"
+          color="primary"
+          size="medium"
+          style={{float: 'left'}}
+        />
+        <Paragraph style={{paddingTop: '2px'}}>
+          URL Slug: <em>{contentFieldSlug.getValue()}</em>
+        </Paragraph>
 
           {statText.map(stat => (
-            <Table>
-              <TableHead>{stat.title}</TableHead>
-              {stat.data && (
-                <>
-                  {console.log(`dummy man: `, stat.data)}
-                  {console.log(`dummy man 2: `, JSON.stringify(stat.data[0]))}
-                  {(stat.data).map(item => (
-                    renderTable(item)
-                  ))}
-                </>
-              )}
-            </Table>
+            <div style={{ padding: '0 2px', marginTop: '12px' }}>
+              <Table style={{background: '#ffffff'}}>
+                <TableHead>
+                  <TableRow>
+                    <TableCell style={{ padding: '10px'}} colSpan="2">
+                    {stat.title}
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+                {stat.data && (
+                  <>
+                    {renderTable(stat.data[0])}
+                  </>
+                )}
+              </Table>
+            </div>
           ))}
-      </Note>
+      </div>
     </>
   );
 };
