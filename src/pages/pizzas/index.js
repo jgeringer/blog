1d71729bf7e5b8a4b2f3405e9a6b18925e51402b
Joseph Geringer <joegeringer@gmail.com>
2022-01-01 21:34:19 -0600

    feat(Pizzas): added filters to plp

--- src/pages/pizzas/index.js
+++ src/pages/pizzas/index.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Link, graphql } from 'gatsby'
 import * as styles from './style.module.css'
 
 export default function Pizzas({ data }) {
     console.log(`pizza time: `, data);
+    
     const allPizzarias = data.allPizzarias.nodes;
     const pizzarias = data.pizzarias.nodes;
 
@@ -11,7 +12,7 @@ export default function Pizzas({ data }) {
 
     // get all pizzas from pizzarias...
 
-    const pizzas = []
+    const initialPizzas = []
     pizzarias.forEach(pizza => {
         const pizzariaPizzas = pizza.pizzas
         const numOfPizzasInPizzaria = pizzariaPizzas.length
@@ -21,21 +22,46 @@ export default function Pizzas({ data }) {
                 pizzaPie['restaurant'] = pizza.title
                 pizzaPie['hasRcCola'] = pizza.hasRcCola
                 pizzaPie['pizzariaSlug'] = pizza.slug
-                pizzas.push(pizzaPie)
+                pizzaPie['area'] = pizza.area
+                initialPizzas.push(pizzaPie)
             })
         } else {
             pizzariaPizzas[0]['restaurant'] = pizza.title
             pizzariaPizzas[0]['hasRcCola'] = pizza.hasRcCola
             pizzariaPizzas[0]['pizzariaSlug'] = pizza.slug
-            pizzas.push(pizzariaPizzas[0])
+            pizzariaPizzas[0]['area'] = pizza.area
+            initialPizzas.push(pizzariaPizzas[0])
         }
     });
-    console.log('pizzas:::: ', pizzas)
+    console.log('initialPizzas:::: ', initialPizzas)
+
+    const [pizzas, setPizzas] = useState(initialPizzas)
+    const [activePizzaria, setActivePizzaria] = useState(pizzarias.length === 1 ? pizzarias[0] : null)
+
   
-    const filterByRcCola = (val) => {
-        console.log('changed')
-        console.log(val)
-        pizzas.filter(x => x.hasRcCola === val)
+    const filterByRcCola = (e) => {
+        const isChecked = e.target.checked
+        if (isChecked) {
+            setPizzas(
+                initialPizzas.filter(x => x.hasRcCola === isChecked)
+            )
+        } else {
+            setPizzas(initialPizzas)
+        }
+    }
+
+    const filterByArea = (e) => {
+        const areaSelected = e.target.value
+        console.log('areaSelected: ', areaSelected)
+        if (areaSelected !== "All Areas") {
+            console.log('not null')
+            setPizzas(
+                initialPizzas.filter(y => y.area === areaSelected)
+            )
+        } else {
+            console.log('show all pizzas')
+            setPizzas(initialPizzas)
+        }
     }
 
     return(
@@ -43,41 +69,16 @@ export default function Pizzas({ data }) {
             <h2>Pizza time</h2>
             <section className='u-flex'>
                 <div className='u-lg-1of4'>
-                    Pizza filters:
+                    Pizzaria filters:
                     <ul>
-                        <li>Topping: Sausage, Crust, Grease</li>
-                        <li>Location: South Side, North Side, Burbs</li>
-                        <li>Nearest</li>
-                        <li>Cut: Square Cut, Pie Cut</li>
-                        <li>My Rating</li>
-                        <li>Style: Tavern, Deep Dish, Cracker, Neopolitan</li>
-                        <li>Packaging: Paper bag, Box</li>
-                        <li>Cash Only</li>
-                        <li>Includes RC:
-                            <br />
-                            <form>
-                                <input type="radio" id="rc_no" name="hasRcCola" value="false" onChange={filterByRcCola(false)} />
-                                <label for="rc_no">NO</label><br />
-                                <input type="radio" id="rc_yes" name="hasRcCola" value="true" onChange={filterByRcCola(true)} />
-                                <label for="rc_yes">YES</label><br />
-                            </form>
-                        </li>
-                        <li>Age: 10+, 20+, 30+, 40+</li>
-                        <li>Eatability factor: (score decreases after x minutes)</li>
                         <li>
-                            <select>
-                                <option>Geos</option>
-                                <option>Palermos</option>
-                                <option>Geovanis</option>
-                            </select>
-                            <br />
-                            <Link to={`/pizzas`}>
-                                Pizzaria: ALL
+                            <Link to={`/pizzas`} className={styles.pizzaria}>
+                                All Pizzarias
                             </Link>
                             {allPizzarias.map(pizzaria => (
                                 <div key={pizzaria.id}>
-                                    <Link to={`/pizzas/${pizzaria.slug}`}>
-                                    Pizzaria: {pizzaria.title} 
+                                    <Link to={`/pizzas/${pizzaria.slug}`} className={styles.pizzaria}>
+                                    {pizzaria.title} 
                                     {pizzaria.hasRcCola && (
                                         <i> (RC)</i>
                                     )}
@@ -85,6 +86,33 @@ export default function Pizzas({ data }) {
                                 </div>
                             ))}
                         </li>
+                        <li> --- </li>
+
+                        {!activePizzaria && (
+                            <>
+                                <li>
+                                    <input type="checkbox" id="hasRcCola" name="hasRcCola" onChange={(e) => filterByRcCola(e)} />
+                                    <label for="hasRcCola">Includes RC</label>
+                                </li>
+                                <li>
+                                    <select onChange={(e) => filterByArea(e)}>
+                                        <option>All Areas</option>
+                                        <option value="North Side">North Side</option>
+                                        <option value="South Side">South Side</option>
+                                        <option value="Burbs">Burbs</option>
+                                    </select>
+                                </li>
+                                <li>---</li>
+                                <li>Nearest</li>
+                                <li>Cut: Square Cut, Pie Cut</li>
+                                <li>My Rating</li>
+                                <li>Style: Tavern, Deep Dish, Cracker, Neopolitan</li>
+                                <li>Unique flavor: Greasy, Sweet Sauce</li>
+                                <li>Packaging: Paper bag, Box</li>
+                                <li>Cash Only</li>
+                                <li>Age: 10+, 20+, 30+, 40+</li>
+                            </>
+                        )}                        
                     </ul>
                 </div>
                 <div className='u-lg-3of4'>
@@ -92,12 +120,13 @@ export default function Pizzas({ data }) {
                         <img src="tmnt-pizza.gif" />
                         COMING SOON
                     </p>
-                    <p>Product Card:</p>
-                    <ul>
-                        <li>Photo</li>
-                        <li>Icons for each of the filters</li>
-                        <li>Phone, Address</li>
-                    </ul>
+                    {activePizzaria && (
+                        <div>
+                            <p>Pizzaria details:
+                                Address: {activePizzaria.address}
+                            </p>
+                        </div>
+                    )}
                     <div>
                         {pizzas.map(pizza => (
                             <div key={pizza.id}>
@@ -123,7 +152,19 @@ export const query = graphql`
                 title
                 slug
                 cashOnly
+                description {
+                    raw
+                }
                 hasRcCola
+                location {
+                    lat
+                    lon
+                }
+                phone
+                url
+                yearEstablished
+                address
+                area
                 pizzas {
                     id
                     title
@@ -138,7 +179,19 @@ export const query = graphql`
                 title
                 slug
                 cashOnly
+                description {
+                    raw
+                }
                 hasRcCola
+                location {
+                    lat
+                    lon
+                }
+                phone
+                url
+                yearEstablished
+                address
+                area
                 pizzas {
                     id
                     title
