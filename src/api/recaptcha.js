46b58683f6630c52be424d3a120a257b8fc11cff
Joseph Geringer <joegeringer@gmail.com>
2023-07-20 16:42:32 -0500

    recaptcha testing on prod

--- src/api/recaptcha.js
+++ src/api/recaptcha.js
@@ -1,7 +1,37 @@
-export default function handler(req, res) {
-    res.status(200).json({ hello: `world` })
+import fetch from "node-fetch"
 
-    
+export default function handler(req, res) {
+    // res.status(200).json({ hello: `world` })
+
+    console.log(`req:`, req);
+
+    // https://www.geeksforgeeks.org/how-to-verify-recaptcha-in-node-js-server-call/
+    const response_key = req.body["g-recaptcha-response"];
+
+    const secret_key = "6LdpGdYUAAAAAFW_hG8BcMNamliqxiZvzD5XVxmk";
+
+    const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secret_key}&response=${response_key}`;
+
+    fetch(url, {
+      method: "post",
+    })
+      .then((response) => response.json())
+      .then((google_response) => {
+   
+        // google_response is the object return by
+        // google as a response
+        if (google_response.success == true) {
+          //   if captcha is verified
+          return res.send({ response: "Successful!" });
+        } else {
+          // if captcha is not verified
+          return res.send({ response: "Failed!" });
+        }
+      })
+      .catch((error) => {
+          // Some error while verify captcha
+        return res.json({ error });
+      });
 
   }
   
